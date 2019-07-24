8d48072aeacccf3b09e496b6922ff11665e31e5a
王海超 <734692249@qq.com>
2019-07-24 16:23:40 +0800

    修改了二元数组的查找

--- Two-dimensional.py
+++ Two-dimensional.py
@@ -6,28 +6,29 @@ class Solution:
     """
     @staticmethod
     def two_dimensional(array, number: int) -> bool:
-        columns = len(array)
-        if columns == 0:
+        if array is not None:
+            columns = len(array)
+            if columns == 0:
+                return False
+            rows = len(array[0])
+            if rows > 0:
+                columns = 0
+                rows -= 1
+                while columns < len(array) and rows >= 0:
+                    if array[columns][rows] == number:
+                        return True
+                    elif array[columns][rows] < number:
+                        columns += 1
+                    else:
+                        rows -= 1
+                return False
             return False
-        rows = len(array[0])
-        if rows > 0:
-            columns = 0
-            rows -= 1
-            while columns < len(array) and rows >= 0:
-                if array[columns][rows] == number:
-                    return True
-                elif array[columns][rows] < number:
-                    columns += 1
-                    print(array[columns][rows])
-                else:
-                    rows -= 1
-                    print(array[columns][rows])
+        else:
             return False
-        return False
 
 
 if __name__ == '__main__':
     array = [[1, 2, 8, 9], [2, 4, 9, 12], [4, 7, 10, 13], [6, 8, 11, 15]]
-    test = Solution.two_dimensional(array, 7)
+    test = Solution.two_dimensional(array, 14)
     print(test)
     print(Solution())
